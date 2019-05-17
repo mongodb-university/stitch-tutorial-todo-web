f086cabe5229b29823ce3c0d2cd84555044d937b
Nick Larew <nick.larew@10gen.com>
2019-05-17 16:13:55 -0400

    Step 3: Set Up Anonymous User Login

--- src/stitch/authentication.js
+++ src/stitch/authentication.js
@@ -2,17 +2,23 @@ import { AnonymousCredential } from "mongodb-stitch-browser-sdk";
 import { app } from "./app.js";
 
 export function loginAnonymous() {
-  // TODO: Allow users to log in anonymously
+  // Allow users to log in anonymously
+  const credential = new AnonymousCredential();
+  return app.auth.loginWithCredential(credential);
 }
 
 export function hasLoggedInUser() {
-  // TODO: Check if there is currently a logged in user
+  // Check if there is currently a logged in user
+  return app.auth.isLoggedIn;
 }
 
 export function getCurrentUser() {
-  // TODO: Return the user object of the currently logged in user
+  // Return the user object of the currently logged in user
+  return app.auth.isLoggedIn ? app.auth.user : null;
 }
 
 export function logoutCurrentUser() {
-  // TODO: Logout the currently logged in user
+  // Logout the currently logged in user
+  const user = getCurrentUser();
+  return app.auth.logoutUserWithId(user.id);
 }
