7086541756c417e6231f440db1f5f54be649050e
Nick Larew <nick.larew@10gen.com>
2019-05-17 16:11:51 -0400

    Step 1: Connect to Stitch

--- src/stitch/app.js
+++ src/stitch/app.js
@@ -4,6 +4,8 @@ import { Stitch } from "mongodb-stitch-browser-sdk";
 const APP_ID = "<YOUR APP ID>";
 
 // TODO: Initialize the app client
-const app =
+const app = Stitch.hasAppClient(APP_ID)
+  ? Stitch.getAppClient(APP_ID)
+  : Stitch.initializeAppClient(APP_ID);
 
 export { app };
