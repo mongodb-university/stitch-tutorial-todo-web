743d7cd0c86f8fd12800018814262677b8ed12f4
Julia Oppenheim <julia.opp23@gmail.com>
2019-07-08 17:37:12 -0400

    (DOCSP-5986): Add Google OAuth to Web ToDo

--- src/components/StitchAuth.js
+++ src/components/StitchAuth.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PropTypes from "prop-types";
 import {
   hasLoggedInUser,
   loginAnonymous,
+  loginGoogle, 
   logoutCurrentUser,
   getCurrentUser,
+  addAuthenticationListener,
+  removeAuthenticationListener,
+  handleOAuthRedirects,
 } from "./../stitch/authentication";
 
 // Create a React Context that lets us expose and access auth state
@@ -13,12 +17,13 @@ const StitchAuthContext = React.createContext();
 
 // Create a React Hook that lets us get data from our auth context
 export function useStitchAuth() {
+
   const context = React.useContext(StitchAuthContext);
   if (!context) {
     throw new Error(`useStitchAuth must be used within a StitchAuthProvider`);
   }
   return context;
-}
+} 
 
 // Create a component that controls auth state and exposes it via
 // the React Context we created.
@@ -28,51 +33,74 @@ export function StitchAuthProvider(props) {
     currentUser: getCurrentUser(),
   });
 
-  // Authentication Actions
-  const handleAnonymousLogin = async () => {
-    const { isLoggedIn } = authState;
-    if (!isLoggedIn) {
-      const loggedInUser = await loginAnonymous();
-      setAuthState({
-        ...authState,
-        isLoggedIn: true,
-        currentUser: loggedInUser,
-      });
-    }
-  };
-  const handleLogout = async () => {
-    const { isLoggedIn } = authState;
-    if (isLoggedIn) {
-      await logoutCurrentUser();
-      setAuthState({
-        ...authState,
-        isLoggedIn: false,
-        currentUser: null,
-      });
+  useEffect(() => {
+    const authListener = {
+      onUserLoggedIn: (auth, loggedInUser) => {
+        if (loggedInUser) {
+          setAuthState(authState => ({
+            ...authState,
+            isLoggedIn: true,
+            currentUser: loggedInUser,
+          }));
+        }
+      },
+      onUserLoggedOut: (auth, loggedOutUser) => {
+        setAuthState(authState => ({
+          ...authState,
+          isLoggedIn: false,
+          currentUser: null,
+        }));
+      }
+    };
+    addAuthenticationListener(authListener);
+    handleOAuthRedirects(); 
+    setAuthState(state => ({ ...state}));
+    return () => {
+      removeAuthenticationListener(authListener);
+    };
+  }, []);
+
+  const handleLogin = React.useCallback(
+    async provider => {
+      if (!authState.isLoggedIn) {
+        switch(provider) {
+          case "anonymous": return loginAnonymous()
+          case "google": return loginGoogle()
+          default: {}
+        }
+      }
+    },
+    [authState],
+  );
+  const handleLogout = React.useCallback(async () => {
+    if (authState.isLoggedIn) {
+      logoutCurrentUser();
     } else {
       console.log(`can't handleLogout when no user is logged in`);
     }
-  };
+  }, [authState]);
 
   // We useMemo to improve performance by eliminating some re-renders
-  const authInfo = React.useMemo(
-    () => {
-      const { isLoggedIn, currentUser } = authState;
-      const value = {
-        isLoggedIn,
-        currentUser,
-        actions: { handleAnonymousLogin, handleLogout },
-      };
-      return value;
-    },
-    [authState.isLoggedIn],
-  );
+  const authInfo = React.useMemo(() => {
+    const { isLoggedIn, currentUser } = authState;
+    const value = {
+      isLoggedIn,
+      currentUser,
+      actions: {
+        handleLogin,
+        handleLogout,
+      },
+    };
+    return value;
+  }, [authState, handleLogin, handleLogout]);
+
   return (
     <StitchAuthContext.Provider value={authInfo}>
       {props.children}
     </StitchAuthContext.Provider>
   );
 }
+
 StitchAuthProvider.propTypes = {
   children: PropTypes.element,
 };
