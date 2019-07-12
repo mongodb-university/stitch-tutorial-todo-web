9a6b5f9e868b42b5c4dde7da050a8ebd47f41980
Julia Oppenheim <julia.opp23@gmail.com>
2019-07-12 16:08:30 -0400

    app ID removed

--- src/stitch/app.js
+++ src/stitch/app.js
@@ -1,8 +1,7 @@
 import { Stitch } from "mongodb-stitch-browser-sdk";
 
 // TODO: Add your Stitch app's App ID
-const APP_ID = "todoweb-snnak";
-// const APP_ID = "<YOUR APP ID>";
+const APP_ID = "<YOUR APP ID>";
 
 // TODO: Initialize the app client
 const app = Stitch.hasAppClient(APP_ID)
